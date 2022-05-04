c1ad9c63789673301faeb43ca64fdbe7177a7bb2
Gabriel Maranhão <gabrielmaranhao48@gmail.com>
2022-05-04 13:47:36 -0300

    fazendo o ajuste no defeito do código

--- Entrega s6-07/index.js
+++ Entrega s6-07/index.js
@@ -5,32 +5,31 @@ function add(n1,n2){
 //.kata2
 function multiply(num1,num2){
     let resultado = 0;
-    for(let i = 0;i<=num2;i++){
-        resultado = add(num1,num1*num2 - num1)
+    for(let i = 1;i<=num2;i=add(i,1)){
+    resultado = add(num1,resultado);
     }
     return resultado
 }
 //.kata3
 function power(x,n){
     let resultado = 1 ;
-    for(let i =0;i<n;i++){
-    resultado = resultado * multiply(x,1)
+    for(let i =0;i<n;i=add(i,1)){
+    resultado =  multiply(x,resultado)
     }
+    return resultado
 }
 //.kata4
 function factorial(z){
-    let resultado = []
-    for(let i = 0;i<=z;i++){
-        resultado.push(i)
+    let resultado = 1;
+    for(let i = 1;i<=z;i=add(i,1)){
+        resultado = multiply(i,resultado)
 
     }
-    let factorial = 1
-    for(let i = 1;i<resultado.length;i++){
-        factorial = factorial *[i] 
-
-    }
-return factorial
+return resultado
 }
+
+
+
 
 
 
